5eca5c441f3db36b13dd3d86faeb888789ce1c13
Ian Billinge <ian.billinge@gmail.com>
2023-12-29 20:48:40 -0500

    Implemented a workable get basis function.

--- peak_resolver/get_models_bases.py
+++ peak_resolver/get_models_bases.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pickle
+import xarray as xr
 
 def get_models(root_dir:str, acids:list):
     mods = {}
@@ -13,3 +14,20 @@ def get_models(root_dir:str, acids:list):
         mods[acid[:3]] = mod
     
     return mods
+    
+def get_basis_functions(fp:str, 
+                        conc:float = 0.5, 
+                        coords_list: tuple = ('Acetic', 'Propanoic', 'Butyric', 'Valeric', 'Caproic'),
+                        order = 'ds'
+                       ):
+    data = xr.load_dataset(fp)
+
+    basis_funcs = []
+    for coord in coords_list:
+        x = data.sel({coord:conc}).dropna(dim='t', how='all')[order]
+        basis_funcs.append(x)
+
+    
+
+
+    return basis_funcs
